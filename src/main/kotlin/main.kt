6b5b674bbc4259ce4b82a31ff0db8a5dcaed9439
jhs512 <jangka512@gmail.com>
2021-04-26 12:33:52 +0900

    Add files via upload

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,19 @@
 import java.text.SimpleDateFormat
 
 fun main() {
-    println("== 게시판 관리 프로그램 시작 ==")
+    println("== 게시물 관리 프로그램 시작 ==")
 
     makeTestArticles()
 
-    while (true) {
+    loop@ while (true) {
         print("명령어) ")
         val command = readLineTrim()
+        val commandBits = command.split(" ")
 
         when {
             command == "system exit" -> {
-                println("프로그램을 종료합니다.")
-                break
+                println("프로그램 종료")
+                break@loop
             }
             command == "article write" -> {
                 print("제목 : ")
@@ -24,77 +25,38 @@ fun main() {
 
                 println("${id}번 게시물이 작성되었습니다.")
             }
-            command.startsWith("article list ") -> {
-                val commandBits = command.trim().split(" ")
-
-                // article list =>
-                // article list 제목 5
-                // article list 4
-
+            command.startsWith("article list") -> {
                 var page = 1
                 var searchKeyword = ""
 
-                // article list 제목_1 1
-                // article list 4
+                if (commandBits.size == 3) {
+                    page = commandBits[2].toInt()
+                }
 
                 if (commandBits.size == 4) {
                     searchKeyword = commandBits[2]
                     page = commandBits[3].toInt()
-                } else if (commandBits.size == 3) {
-                    page = commandBits[2].toInt()
                 }
 
-                val itemsCountInAPage = 5
-                val offsetCount = (page - 1) * itemsCountInAPage
+                println("번호 / 작성날짜 / 갱신날짜 / 제목")
 
-                val filteredArticles = getFilteredArticles(searchKeyword, offsetCount, itemsCountInAPage)
+                val takeCount = 10
+                val offsetCount = (page - 1) * takeCount
 
-                println("번호 / 작성날짜 / 제목")
+                val filteredArticles = getFilteredArticles(searchKeyword, offsetCount, takeCount)
 
                 for (article in filteredArticles) {
-                    println("${article.id} / ${article.regDate} / ${article.title}")
+                    println("${article.id} / ${article.regDate} / ${article.updateDate} / ${article.title}")
                 }
-            }
-            command.startsWith("article delete ") -> {
-                val id = command.trim().split(" ")[2].toInt()
-
-                val articleToDelete = getArticleById(id)
-
-                if (articleToDelete == null) {
-                    println("${id}번 게시물은 존재하지 않습니다.")
-                    continue
-                }
-
-                articles.remove(articleToDelete)
-
-                println("${id}번 게시물을 삭제하였습니다.")
-            }
-            command.startsWith("article modify ") -> {
-                val id = command.trim().split(" ")[2].toInt()
-
-                val articleToModify = getArticleById(id)
-
-                if (articleToModify == null) {
-                    println("${id}번 게시물은 존재하지 않습니다.")
-                    continue
-                }
-
-                print("${id}번 게시물 새 제목 : ")
-                articleToModify.title = readLineTrim()
-                print("${id}번 게시물 새 내용 : ")
-                articleToModify.body = readLineTrim()
-                articleToModify.updateDate = Util.getNowDateStr()
-
-                println("${id}번 게시물을 수정하였습니다.")
             }
             command.startsWith("article detail ") -> {
-                val id = command.trim().split(" ")[2].toInt()
+                val id = commandBits[2].toInt()
 
                 val article = getArticleById(id)
 
                 if (article == null) {
                     println("${id}번 게시물은 존재하지 않습니다.")
-                    continue
+                    continue@loop
                 }
 
                 println("번호 : ${article.id}")
@@ -103,16 +65,51 @@ fun main() {
                 println("제목 : ${article.title}")
                 println("내용 : ${article.body}")
             }
+            command.startsWith("article delete ") -> {
+                val id = commandBits[2].toInt()
+
+                val article = getArticleById(id)
+
+                if (article == null) {
+                    println("${id}번 게시물은 존재하지 않습니다.")
+                    continue@loop
+                }
+
+                articles.remove(article)
+
+                println("${id}번 게시물을 삭제하였습니다.")
+            }
+            command.startsWith("article modify ") -> {
+                val id = commandBits[2].toInt()
+
+                val article = getArticleById(id)
+
+                if (article == null) {
+                    println("${id}번 게시물은 존재하지 않습니다.")
+                    continue@loop
+                }
+
+                print("${id}번 게시물 새 제목 : ")
+                val title = readLineTrim()
+                print("${id}번 게시물 새 내용 : ")
+                val body = readLineTrim()
+
+                article.title = title
+                article.body = body
+                article.updateDate = Util.getNowDateStr()
+
+                println("${id}번 게시물을 수정하였습니다.")
+            }
             else -> {
-                println("`$command` 은(는) 존재하지 않는 명령어 입니다.")
+                println("`$command`(은)는 존재하지 않는 명령어 입니다.")
             }
         }
     }
 
-    println("== 게시판 관리 프로그램 끝 ==")
+    println("== 게시물 관리 프로그램 끝 ==")
 }
 
-/* 게시물 관련 시작 */
+// 게시물 관련 시작
 data class Article(
     val id: Int,
     val regDate: String,
@@ -120,10 +117,6 @@ data class Article(
     var title: String,
     var body: String
 )
-
-var articlesLastId = 0
-
-val articles = mutableListOf<Article>()
 
 fun getArticleById(id: Int): Article? {
     for (article in articles) {
@@ -133,6 +126,22 @@ fun getArticleById(id: Int): Article? {
     }
 
     return null
+}
+
+fun addArticle(title: String, body: String): Int {
+    val id = articlesLastId + 1
+    val regDate = Util.getNowDateStr()
+    val updateDate = Util.getNowDateStr()
+    articles.add(Article(id, regDate, updateDate, title, body))
+    articlesLastId = id
+
+    return id
+}
+
+fun makeTestArticles() {
+    for (id in 1..100) {
+        addArticle("제목_$id", "내용_$id")
+    }
 }
 
 fun getFilteredArticles(searchKeyword: String, offsetCount: Int, takeCount: Int): List<Article> {
@@ -148,14 +157,14 @@ fun getFilteredArticles(searchKeyword: String, offsetCount: Int, takeCount: Int)
         }
     }
 
+    val filtered2Articles = mutableListOf<Article>()
+
     val startIndex = filtered1Articles.lastIndex - offsetCount
-    var endIndex = startIndex - takeCount + 1
+    var endIndex = startIndex - (takeCount - 1)
 
     if (endIndex < 0) {
         endIndex = 0
     }
-
-    val filtered2Articles = mutableListOf<Article>()
 
     for (i in startIndex downTo endIndex) {
         filtered2Articles.add(filtered1Articles[i])
@@ -164,37 +173,18 @@ fun getFilteredArticles(searchKeyword: String, offsetCount: Int, takeCount: Int)
     return filtered2Articles
 }
 
-fun makeTestArticles() {
-    for (id in 1..1000) {
-        val title = "제목_$id"
-        val body = "내용_$id"
+val articles = mutableListOf<Article>()
+var articlesLastId = 0
+// 게시물 관련 끝
 
-        addArticle(title, body)
-    }
-}
-
-fun addArticle(title: String, body: String): Int {
-    val id = articlesLastId + 1
-    val regDate = Util.getNowDateStr()
-    val updateDate = Util.getNowDateStr()
-
-    val article = Article(id, regDate, updateDate, title, body)
-    articles.add(article)
-
-    articlesLastId = id
-
-    return id
-}
-/* 게시물 관련 끝 */
-
-/* 유틸관련 시작 */
+// 유틸관련 시작
 fun readLineTrim() = readLine()!!.trim()
 
 object Util {
     fun getNowDateStr(): String {
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 
-        return dateFormat.format(System.currentTimeMillis())
+        return format.format(System.currentTimeMillis())
     }
 }
-/* 유틸관련 끝 */
+// 유틸관련 끝
